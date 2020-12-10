7fb6b8f2cf16dd480a750118c87b81895e7a3082
ivan-dev <admin@stepanov.one>
2020-12-10 19:24:55 +0300

    Second

--- test_add_group.py
+++ test_add_group.py
@@ -1,41 +1,62 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class AddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome(r'D:\chromedriver')
         self.wd.implicitly_wait(30)
 
     
-    def test_add_group(self):
+    def add_group(self):
         wd = self.wd
-        wd.get("http://localhost:8080/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_main_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.create_group(wd, name="Test", header="Testing group", footer="Testing group")
+        self.return_group_page(wd)
+        self.logout(wd)
+
+    def add_empty_group(self):
+        wd = self.wd
+        self.open_main_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.create_group(wd, name="", header="", footer="")
+        self.return_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd, name, header, footer):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Test")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("Testing group")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("Testing group")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, wd, login, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_main_page(self, wd):
+        # переход на главную страницу и авторизация
+        wd.get("http://localhost:8080/group.php")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
