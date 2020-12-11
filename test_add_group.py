47c336f34064677c181fedb37e6c495eee70008a
ivan-dev <admin@stepanov.one>
2020-12-11 07:13:52 +0300

    Переделака для новой версии addressbook 9.0.1

--- test_add_group.py
+++ test_add_group.py
@@ -12,12 +12,11 @@ class AddGroup(unittest.TestCase):
         self.wd = webdriver.Chrome(r'D:\chromedriver')
         self.wd.implicitly_wait(30)
 
-
-    def add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_main_page(wd)
         self.login(wd, login="admin", password="secret")
-        self.create_group(wd, Group(name="Test", header="Testing group", footer="Testing group"))
+        self.create_group(wd, Group(name=u"Тестовая", header=u"Тестовая Header", footer=u"Тестовая  Footer"))
         self.return_group_page(wd)
         self.logout(wd)
 
@@ -30,13 +29,13 @@ class AddGroup(unittest.TestCase):
         self.logout(wd)
 
     def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_link_text(u"Выйти").click()
 
     def return_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text(u"Группы").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -54,25 +53,29 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("user").send_keys(login)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd.find_element_by_xpath(u"//input[@value='Войти']").click()
 
     def open_main_page(self, wd):
-        # переход на главную страницу и авторизация
-        wd.get("http://localhost:8080/group.php")
+        # переход на главную страницу
+        wd.get("http://localhost:8080/")
 
     def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
-    
+
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
