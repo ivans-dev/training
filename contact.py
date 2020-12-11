b0d8bdd2aa17586224c7952d34d70b2c03091208
ivan-dev <admin@stepanov.one>
2020-12-11 09:55:40 +0300

    Переделака для новой версии addressbook 9.0.1

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, firstname, lastname, middlename, title):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.middlename = middlename
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
+from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from contact import Contact
 from group import Group
 
 
-class AddGroup(unittest.TestCase):
+class TestABook(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Chrome(r'D:\chromedriver')
         self.wd.implicitly_wait(30)
@@ -27,13 +28,56 @@ class AddGroup(unittest.TestCase):
         self.create_group(wd, Group(name="", header="", footer=""))
         self.return_group_page(wd)
         self.logout(wd)
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_main_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.create_contact(wd, Contact(firstname="Иванов", lastname="Иван", middlename="Иванович", title="Менеджер"))
+        self.get_address(wd)
+        self.add_contact_info(wd)
+        self.get_main(wd)
+        self.logout(wd)
 
+    #Добавление информации о контакте
+    def add_contact_info(self, wd):
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("theform").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    #Добавление адреса при заполнении информации о контакте
+    def get_address(self, wd):
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(u"Россия, Тульская область, г. Тула")
+        wd.find_element_by_xpath("(//input[@name='quickadd'])[2]").click()
+
+    #Переход на главную страницу
+    def get_main(self, wd):
+        wd.find_element_by_link_text(u"Главная").click()
+
+    #Создание контакта
+    def create_contact(self, wd):
+        wd.find_element_by_link_text(u"Добавить контакт").click()
+
+    #Выход
     def logout(self, wd):
         wd.find_element_by_link_text(u"Выйти").click()
 
+    #Переход в группы
     def return_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
+    # Создание группы
     def create_group(self, wd, group):
         wd.find_element_by_link_text(u"Группы").click()
         wd.find_element_by_name("new").click()
@@ -47,6 +91,7 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
+    #Авторизация
     def login(self, wd, login, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -55,6 +100,7 @@ class AddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath(u"//input[@value='Войти']").click()
 
+    #Открытие главной страницы
     def open_main_page(self, wd):
         # переход на главную страницу
         wd.get("http://localhost:8080/")
