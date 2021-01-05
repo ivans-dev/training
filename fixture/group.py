dbbbb687235b87312d9978e33f40e2d3c08753a4
ivan-dev <admin@stepanov.one>
2021-01-05 22:30:58 +0300

    Сохранение

--- fixture/group.py
+++ fixture/group.py
@@ -47,3 +47,8 @@ class GroupHelper:
             wd.find_element_by_name(field_name).click()
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_pages()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,4 +2,6 @@ from model.group import Group
 
 
 def test_edit_group(app):
-    app.group.edit_first(Group(name=u"Тестовая 1"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+    app.group.edit_first(Group(name="Тестовая 1"))
