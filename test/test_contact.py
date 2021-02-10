729f000f67bab8e1bd1317ed00f1f16e43d03c54
vats-ui-old <iastepanov@rt-dc.ru>
2021-02-10 07:10:28 +0300

    21

--- test/test_contact.py
+++ test/test_contact.py
@@ -1,11 +1,13 @@
 import re
 
 
-def test_address_on_home_page(app,db):
+def test_address_on_home_page(app, db):
     contacts = app.contact.get_contacts_list()
     for contact in contacts:
         contact_from_home_page = contact
-        assert contact_from_home_page.address == db.get_info_contact("address", contact.id)[0]
+        adr2 = db.get_info_contact("address", contact.id)[0]
+        adr1 = contact_from_home_page.address
+        assert adr1 == adr2
 
 
 def test_emails_on_home_page(app, db):
@@ -14,8 +16,6 @@ def test_emails_on_home_page(app, db):
         contact_from_home_page = contact
         assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(
             list(db.get_info_contact("mail", contact.id)[0]))
-
-
 
 
 def test_name_on_home_page(app, db):
@@ -34,16 +34,6 @@ def test_phones_on_home_page(app, db):
             list(db.get_info_contact("phone", contact.id)[0]))
 
 
-#
-# def test_phones_on_contact_view_page(app):
-#     contacts = app.contact.get_contacts_list()
-#     index = randrange(len(contacts))
-#     contact_from_view_page = app.contact.get_contact_from_view_page(index)
-#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-#     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
-#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
 
 
 def clear(s):
